3b1f78c4383ff302d5351dde665a22a3f17afdf1
kangjs3779 <123609533+kangjs3779@users.noreply.github.com>
2023-02-14 10:54:42 +0900

    0214 first commit

---
+++ src/ch01/sec11/MainShortCut.java
@@ -0,0 +1,16 @@
+package ch01.sec11;
+
+public class MainShortCut {
+	public static void main(String[] args) {
+		System.out.println("Hi java");
+		System.out.println("출력되는 내용");
+		System.out.println("단축키로 출력하기이이이이");
+		
+		//주석 컨트롤 /
+		// 실행 컨트롤 f11
+		//창크기 컨트롤 m
+		//한줄 지우기 컨트롤 d
+		//줄 옮기기 컨트롤 방향키
+		//힌트얻기 컨트롤 스페이스
+	}
+}
